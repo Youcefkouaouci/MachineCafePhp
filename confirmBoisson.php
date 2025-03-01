0d59415af9297655a504615f8ddc1c63402af12a
unknown <youcefkouaouci@gmail.com>
2025-03-01 13:02:08 +0100

    confirm choose coffee need editing

--- confirmBoisson.php
+++ confirmBoisson.php
@@ -3,7 +3,7 @@ require_once("data.php");
 var_dump($_POST);
 
 if (isset($_POST["title"])) {
-    header("Location: choixSucre.php?sugar=$_GET");
+    header("Location: choixSucre.php?sugar=.$_GET");
 } else {
     header("Location: choixBoisson.php?");
 }
